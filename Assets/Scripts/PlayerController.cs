0c47454d75742cc0b5290ac041c4a0ad49745757
Timothy Touch <chaosbounder@gmail.com>
2016-02-16 09:43:04 -0800

    Added Basic Player Movement and Camera Movement
    Added PlayerController script and basic movement
    Allowed collision between WallBlock and Player prefab
    Rescale Player character so it's easier to see
    Added CameraController so that camera will follow player
    Fixed bug in BoardCreator script where Player does not always spawn on
    map generation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float jumpForce;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		//Reference so I don't have to type this long thing out repeatedly
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Use FixedUpdate with Rigidbody
+	void FixedUpdate () {
+		//The "jump" mechanic
+		if (Input.GetKey (KeyCode.W)) {
+			rb2d.AddForce ( new Vector2(rb2d.velocity.x, jumpForce), ForceMode2D.Impulse);
+		}
+			
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		rb2d.velocity = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
+	}
+}
