4fd5207b00a477e9936eb54de7a98900f81c7950
Ken Miller <ken_miller521@yahoo.com>
2016-02-17 11:59:47 -0800

    Title Screen
    The title screen for Adagio. Play button loads a new level and Quit
    brings up a confirmation canvas. Yes button on confirmation closes the
    game and the No button deactivate the canvas bringing the player back to
    the main menu.

---
+++ Assets/Scripts/MainMenuCtrl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuCtrl : MonoBehaviour 
+{
+	public GameObject QuitConfirmationCanvas;
+	// Use this for initialization
+	void Start () 
+	{
+        QuitConfirmationCanvas = GameObject.Find("QuitConfirmationCanvas");
+        QuitConfirmationCanvas.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewGame()
+	{        
+		Application.LoadLevel(1);
+	}
+	public void ConfirmQuit()
+	{
+        QuitConfirmationCanvas.SetActive(true);
+	}
+    public void ConfirmQuitNo()
+    {
+        Debug.Log("PRESSED NO");
+        QuitConfirmationCanvas.SetActive(false);
+    }
+    public void Quit()
+    {
+        Debug.Log("PRESSED YES");
+        Application.Quit ();
+    }
+}
