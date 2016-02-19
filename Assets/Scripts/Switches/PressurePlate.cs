29bd1b515f655b5f9200bb862fca5bf785a389fa
ScotteRoberts <s.e.roberts0@gmail.com>
2016-02-19 15:57:07 -0800

    Creation of Basic PressurePlate/Receiver
    + TestPressurePlate scene
    + PressurePlate sprite
    + PressurePlate script

---
+++ Assets/Scripts/Switches/PressurePlate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Creates a trigger space that the player can walk into.
+ * The receiver attatched to the pressureplate will move when triggered and
+ * move back when the trigger is exited.
+ */
+public class PressurePlate : MonoBehaviour {
+
+	public GameObject receiver;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		receiver.transform.position = new Vector2 (receiver.transform.position.x + 1, receiver.transform.position.y + 1);
+	}
+	void OnTriggerExit2D(Collider2D col)
+	{
+		receiver.transform.position = new Vector2 (receiver.transform.position.x - 1, receiver.transform.position.y - 1);
+	}
+}
