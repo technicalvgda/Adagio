d2d5cb733d0f2e1a11715a89cfda630bbeeb7c21
kdo70 <kdo70@yahoo.com>
2016-02-18 20:30:25 -0800

    Added Options Menu
    Added a options canvas
    Added buttons that lets you go in and out of the options menu
    Renamed Main Menu canva
    Made it more clean when you enter in and out of menus

--- Assets/Scripts/MainMenuCtrl.cs
+++ Assets/Scripts/MainMenuCtrl.cs
@@ -4,12 +4,17 @@ using System.Collections;
 
 public class MainMenuCtrl : MonoBehaviour 
 {
+	public GameObject MainMenuCanvas;
 	public GameObject QuitConfirmationCanvas;
+	public GameObject OptionsCanvas;
 	// Use this for initialization
 	void Start () 
 	{
+		MainMenuCanvas = GameObject.Find ("MainMenuCanvas");
         QuitConfirmationCanvas = GameObject.Find("QuitConfirmationCanvas");
         QuitConfirmationCanvas.SetActive(false);
+		OptionsCanvas = GameObject.Find("OptionsCanvas");
+		OptionsCanvas.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -17,17 +22,29 @@ public class MainMenuCtrl : MonoBehaviour
 	
 	}
 		
+	public void EnterOptions(){
+		OptionsCanvas.SetActive (true);
+		MainMenuCanvas.SetActive (false);
+	}
+
+	public void GoBacktoMainMenu(){
+		OptionsCanvas.SetActive (false);
+		MainMenuCanvas.SetActive (true);
+	}
+
 public void LoadScene(string name) {
 	SceneManager.LoadScene(name); //Application.LoadLevel() is obsolete
 		    					}
 public void ConfirmQuit()
 	{
         QuitConfirmationCanvas.SetActive(true);
+		MainMenuCanvas.SetActive (false);
 	}
     public void ConfirmQuitNo()
     {
         Debug.Log("PRESSED NO");
         QuitConfirmationCanvas.SetActive(false);
+		MainMenuCanvas.SetActive (true);
     }
     public void Quit()
     {
