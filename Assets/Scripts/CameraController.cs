eb580fae3c2ce71074a769454590d89ac101dd6b
Steven McCracken <stevenamccracken@gmail.com>
2016-02-19 18:25:51 -0800

    Revert "Merge branch 'master' into KenMiller_HubRoom"
    This reverts commit 0fa963b02609515eaeb165e38a90136cc20408ea, reversing
    changes made to 624ea024bf29d9d44f57dcade5658bfc3d6ea461.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,11 +5,7 @@ public class CameraController : MonoBehaviour {
 
 	public GameObject player;
 
-    public float smoothTime = 0.5f;
-
 	private Vector3 offset; //Set relative position to player object
-
-    private Vector3 currentVelocity; //Velocity of the camera
 
 	// Use this for initialization
 	void Start () {
@@ -18,16 +14,14 @@ public class CameraController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-    // Changed from LateUpdate to FixedUpdate so that Camera follows character more smoothly.
-	void FixedUpdate () {
+	void LateUpdate () {
 		
-		if (player == null)
-        {
-            player = GameObject.Find("PlayerPlaceholder(Clone)");
-        }
-        else {
-            //Camera's new position is the player's position offsetted
-            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), ref currentVelocity, smoothTime);
+		if (player == null) {
+			player = GameObject.Find ("PlayerPlaceholder(Clone)");
+		} else {
+			//Camera's new position is the player's position offsetted
+			transform.position = player.transform.position + offset;
 		}
+	
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,6 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public float jumpForce;
-    public float jumpSpeed;
-    public bool paused = false;
-    private Vector2 currentVelocity;
 
 	private Rigidbody2D rb2d;
 
@@ -17,27 +14,15 @@ public class PlayerController : MonoBehaviour {
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
-	// Use Update with Rigidbody
-	void Update () {
-        if (!paused)
-        {
-            //The "jump" mechanic
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                //Making it directly alter vertical velocity so jump is instantaneous as well
-                //as not super powerful.
-                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
-                //rb2d.AddForce ( new Vector2(rb2d.velocity.x, jumpForce), ForceMode2D.Impulse);
-            }
+	// Use FixedUpdate with Rigidbody
+	void FixedUpdate () {
+		//The "jump" mechanic
+		if (Input.GetKey (KeyCode.W)) {
+			rb2d.AddForce ( new Vector2(rb2d.velocity.x, jumpForce), ForceMode2D.Impulse);
+		}
+			
+		float moveHorizontal = Input.GetAxis ("Horizontal");
 
-            float moveHorizontal = Input.GetAxis("Horizontal");
-
-            currentVelocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
-            rb2d.velocity = currentVelocity;
-        }
-        else
-        {
-            rb2d.velocity = Vector2.zero;
-        }
+		rb2d.velocity = new Vector2 (moveHorizontal * speed, rb2d.velocity.y);
 	}
 }
