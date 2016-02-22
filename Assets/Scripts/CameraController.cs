379129423ee42ec0d0e1e1d240a71fb1c5830612
fannbrian <fannbrian@gmail.com>
2016-02-22 09:00:46 -0800

    PanSwitch/Button
    +Panning Button : To use, be on the panning button and press 'E'.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-	public GameObject player;
+    public GameObject player;
 
-	private Vector3 offset; //Set relative position to player object
+    public float smoothTime = 0.5f;
 
-	// Use this for initialization
-	void Start () {
-		//Calculates offset by subtracting player position from camera object position
-		offset = transform.position;
-	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
-		
-		if (player == null) {
-			player = GameObject.Find ("PlayerPlaceholder(Clone)");
-		} else {
-			//Camera's new position is the player's position offsetted
-			transform.position = player.transform.position + offset;
-		}
-	
-	}
+    private Vector3 offset; //Set relative position to player object
+
+    private Vector3 currentVelocity; //Velocity of the camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculates offset by subtracting player position from camera object position
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    // Changed from LateUpdate to FixedUpdate so that Camera follows character more smoothly.
+    void FixedUpdate()
+    {
+
+        if (player == null)
+        {
+            player = GameObject.Find("PlayerPlaceholder(Clone)");
+        }
+        else
+        {
+            //Camera's new position is the player's position offsetted
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z), ref currentVelocity, smoothTime);
+        }
+    }
 }
