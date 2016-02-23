3d08b4ae50492cd4fed1b15916cfac982bf189a5
TruAngel <angeltrujillomk2@yahoo.com>
2016-02-23 00:24:54 -0800

    Pressure Plate + Misc.
    -Pressure Plate Switch created and defined.
    -Mask Layers were defined and assigned to the following:
    Player(PlayerPlaceHolder)
    Switch(Switch Collision Trigger Area)
    Block(Floor/wall/ceiling block)
    -The PlayerPlaceHolder prefab was changed so as to not rotate (circle
    collider) when moving.
    -PlayerPlaceHolder prefab was given PlayerGround point so that the
    pressure plates detect it standing on it.
    
    The pressure plate features a rising/sinking block that will sink when
    the player stands on it and rise when it gets off it. The switch will
    activate when the block sinks down a certain depth, like a button. Can
    be placed arbitrarily. As of now only the player can sink the button,
    future updates may allow other obstacles to activate the switch.

---
+++ Assets/SinkButton.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinkButton : MonoBehaviour {
+
+    public Transform sinker;
+    public bool signal = false;
+    Vector3 sinkHeight;     //for the signal to be off
+    Vector3 sinkLow;        //for the signal to be on
+    Vector3 shift;
+    Vector3 speed = new Vector3(0.0f, 0.0f, 0.0f);
+
+    public GameObject PlayerObject;
+    public LayerMask SwitchLayers;
+    public LayerMask PlayerLayer;
+    public bool onPlate;
+    private Transform m_PlayerGround;
+
+
+    // Use this for initialization
+    void Start () {
+
+        sinkHeight = sinker.position;//this sets the original hight of the plate
+        sinkLow = sinker.position;
+        sinkLow.y = (sinkHeight.y - 0.3f);//this sets the pressed state of the plate
+
+        
+
+    }
+
+    void FixedUpdate () {
+
+
+        //signal = false;
+        Debug.Log("Y: " + sinker.transform.localPosition.y);
+        m_PlayerGround = PlayerObject.transform.FindChild("PlayerGround");//defines the point where
+        onPlate = Physics2D.OverlapPoint(m_PlayerGround.position, SwitchLayers);//the collision is detected (player-switch)
+
+        if (onPlate)
+        {
+            Sink();
+        }
+        else
+        {
+            Rise();
+        }
+        //----
+        if ((sinker.transform.localPosition.y) < (-0.1f))
+        {
+            signal = true;
+        }
+        else
+        {
+            signal = false;
+            //Debug.Log("Item has been fixed!");
+        }
+        //----
+        if (signal)
+        {
+            //insert action here
+        }
+        
+
+
+	}
+
+
+
+    void Sink()
+    {
+        shift = Vector3.SmoothDamp((sinker.position), new Vector3((this.transform.localPosition.x), (this.transform.localPosition.y - 0.2f), shift.z), ref (speed), 1.0f);
+        sinker.position = shift;
+    }
+
+    void Rise()
+    {
+        shift = Vector3.SmoothDamp((sinker.position), new Vector3((this.transform.localPosition.x), (this.transform.localPosition.y + 0.1f), shift.z), ref (speed), 1.0f);
+        sinker.position = shift;
+    }
+
+    void Filler()
+    {
+        m_PlayerGround = PlayerObject.transform.FindChild("PlayerGround");//defines the point where
+        onPlate = Physics2D.OverlapPoint(m_PlayerGround.position, SwitchLayers);//the collision is detected (player-switch)
+    }
+
+}
