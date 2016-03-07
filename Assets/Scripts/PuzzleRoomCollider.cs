1744807245d6af54989a2ac2379130ab83fedf40
Timothy Touch <chaosbounder@gmail.com>
2016-03-06 20:04:36 -0800

    Trap Player in Puzzle Room
    -When Player enters a puzzle room, door appears
    -For testing purposes, pressing 'q' opens door

--- Assets/Scripts/PuzzleRoomCollider.cs
+++ Assets/Scripts/PuzzleRoomCollider.cs
@@ -2,10 +2,19 @@
 using System.Collections;
 
 public class PuzzleRoomCollider : MonoBehaviour {
+	public GameObject door;
 
+	void Update(){
+		if(Input.GetKeyDown("q")){
+			Debug.Log("Door opens");
+			door.SetActive (false);
+		}
+	}
 
 	void OnTriggerEnter2D(Collider2D obj) {
 		Debug.Log("Entered Puzzle Room");
+		door.SetActive (true);
+
 	}
 
 }
