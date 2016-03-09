7091f5defad6c09f4d70ffdda0c19419ca7282ec
Doan D <tamitystg@gmail.com>
2016-03-09 05:18:50 -0800

    Pause menu
    Summary:
    - Press ESC or P to bring up the pause menu.
    - Press ESC or P or click on the RESUME button to resume the game.
    - Click on the QUIT button to quit the game.
    - The SETTINGS button does nothing yet, since the settings menu seems to
    be incomplete.

---
+++ Assets/NEW/Pause.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject objFade;
+    public GameObject objText;
+    public GameObject objButtons;
+    Image imgFade;
+    bool busy;
+    bool paused;
+
+    // Use this for initialization
+    void Start ()
+    {
+        imgFade = objFade.GetComponent<Image>();
+        objText.SetActive(false);
+        objButtons.SetActive(false);
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!busy && (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P)))
+        {
+            // Prevent input until transitioning is over
+            busy = true;
+            paused = !paused;
+            if (paused)
+                StartCoroutine(_TransitionIn());
+            else
+                StartCoroutine(_TransitionOut());
+        }
+    }
+
+    IEnumerator _TransitionIn()
+    {
+        // Freeze the game
+        Time.timeScale = 0;
+
+        // Fade in over 10 frames
+        imgFade.color = Color.clear;
+        for (int i = 0; i < 10; i++)
+        {
+            imgFade.color += new Color(0, 0, 0, 0.04f);
+            yield return null;
+        }
+        imgFade.color = new Color(0, 0, 0, 0.4f);
+
+        // Show the PAUSED text and the buttons
+        objText.SetActive(true);
+        objButtons.SetActive(true);
+
+        // Allow input again
+        busy = false;
+    }
+
+    IEnumerator _TransitionOut()
+    {
+        // Resume the game
+        Time.timeScale = 1;
+
+        objText.SetActive(false);
+        objButtons.SetActive(false);
+
+        // Fade out over 10 frames
+        imgFade.color = new Color(0, 0, 0, 0.4f);
+        for (int i = 0; i < 10; i++)
+        {
+            imgFade.color -= new Color(0, 0, 0, 0.04f);
+            yield return null;
+        }
+        imgFade.color = Color.clear;
+
+        // Allow input again
+        busy = false;
+    }
+
+    // This method is called by the Resume button
+    public void Resume()
+    {
+        if (!busy)
+        {
+            busy = true;
+            paused = !paused;
+            StartCoroutine(_TransitionOut());
+        }
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
