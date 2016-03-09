7d92c5bc4be0ddc496cc232a4c0adf047217645d
Christopher Tran <chrishuy.tran@yahoo.com>
2016-03-09 10:56:14 -0800

    Flashing Puzzle Mechanic
    Added FlashScene
    Added Flashing Button Script
    Added Metronome Script
    Added Flashing Pressure Plate Script

---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Metronome : MonoBehaviour {
+    bool on;
+    public GameObject plate;
+    int timing;
+    SpriteRenderer sr;
+
+	void Start ()
+    {
+        sr = this.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(plate.GetComponent<PressurePlateFlash>().stepped)
+        {
+            on = true;
+        }
+        if(on)
+        {
+            this.Blink();
+            timing = (int)Time.time;
+        }
+	}
+
+    void Blink()
+    {
+        if (timing % 2 == 0)
+        {
+            sr.color = new Color(1, 1, 1);
+        }
+        else
+        {
+            sr.color = new Color(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PressurePlateFlash.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlateFlash : MonoBehaviour {
+    public bool stepped;
+    public bool released;
+
+    void Start()
+    {
+        stepped = false;
+        released = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        stepped = true;
+        Debug.Log("On");
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        stepped = false;
+        released = true;
+        Debug.Log("Off");
+    }
+}
