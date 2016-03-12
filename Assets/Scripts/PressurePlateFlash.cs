0e617c9753eb2709211f2119cfae15e0ca0377aa
ScotteRoberts <s.e.roberts0@gmail.com>
2016-03-11 16:23:04 -0800

    PressurePlate turned into Button
    +button timer that resets when button is released.

--- Assets/Scripts/PressurePlateFlash.cs
+++ Assets/Scripts/PressurePlateFlash.cs
@@ -4,14 +4,27 @@ using System.Collections;
 public class PressurePlateFlash : MonoBehaviour {
     public bool stepped;
     public bool released;
-	float timing;
+	float elapsedtime;
 
     void Start()
     {
-		timing = 0f;
+		elapsedtime = 0f;
         stepped = false;
         released = false;
     }
+	void Update()
+	{
+		elapsedtime = Input.GetKeyDown (KeyCode.E) ? 0 : elapsedtime;
+		if (stepped == true && Input.GetKey(KeyCode.E)) {
+			elapsedtime += Time.deltaTime;
+			Debug.Log(elapsedtime);
+		}
+
+		if (elapsedtime > 0.9f && elapsedtime < 1.1f && stepped == true && Input.GetKeyUp (KeyCode.E)) {
+			Debug.Log ("Correct");
+			elapsedtime = 0;
+		}
+	}
 
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -19,18 +32,10 @@ public class PressurePlateFlash : MonoBehaviour {
 		Debug.Log("On");
 		col.attachedRigidbody.sleepMode = RigidbodySleepMode2D.NeverSleep;
     }
-	void OnTriggerStay2D(Collider2D col)
-	{
-		Debug.Log(timing);
-		timing+= Time.deltaTime;
-	}
-
+		
     void OnTriggerExit2D(Collider2D col)
     {
-		if (timing > 0.9f && timing < 1.1f) {
-			Debug.Log ("Correct");
-		}
-		timing = 0;
+		elapsedtime = 0;
         stepped = false;
         released = true;
         Debug.Log("Off");
