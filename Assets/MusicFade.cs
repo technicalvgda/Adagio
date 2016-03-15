99d9e17fc2a5239246a5833656efa1b0a2e53e7e
devinjoseph <devin.joseph@yahoo.com>
2016-03-15 16:13:32 -0700

    Music Fade In/Out
    Starts playing one audio clip, then as time passes, starts to play the
    other clip. NEED to add two audio sources to an game object

---
+++ Assets/MusicFade.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class MusicFade : MonoBehaviour
+{
+
+    public AudioSource track1;
+    public AudioSource track2;
+    public float audio1Volume = 1.0f;
+    public float audio2Volume = 0.0f;
+    public bool track2Playing  = false;
+
+    void Start() {
+        track1.Play();
+
+       
+    }
+ 
+ void Update()
+    {
+        fadeOut();
+
+        if (audio1Volume <= 0.1)
+        {
+            if (track2Playing == false)
+            {
+                track2Playing = true;
+                
+                track2.Play();
+            }
+
+            fadeIn();
+        }
+    }
+
+
+    void fadeIn()
+    {
+        if (audio2Volume < 1)
+        {
+            audio2Volume += 0.1f * Time.deltaTime;
+            track2.volume = audio2Volume;
+        }
+    }
+
+    void fadeOut()
+    {
+        if (audio1Volume > 0.1)
+        {
+            audio1Volume -= 0.1f * Time.deltaTime;
+            track1.volume = audio1Volume;
+        }
+    }
+}
