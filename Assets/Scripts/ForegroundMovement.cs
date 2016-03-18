1775065a6cc98bc688419ebd182ee2e8c5330f7a
Darkchicken <joshshucker@hotmail.com>
2016-03-17 17:27:05 -0700

    Added Foreground and Art Assets

---
+++ Assets/Scripts/ForegroundMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForegroundMovement : MonoBehaviour
+{
+
+    float maxX = 200f;
+    float minX = 0f;
+    float foregroundHeight = 100f;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null)
+        {
+            player = GameObject.Find("Player(Clone)");
+        }
+        else
+        {
+            if (player.transform.position.x > 0 && player.transform.position.x < 200)
+            {
+                float playerValue = player.transform.position.x / 200;
+
+                transform.position = Vector2.Lerp(new Vector2(maxX, foregroundHeight), new Vector2(minX, foregroundHeight), playerValue);
+            }
+
+        }
+    }
+}
