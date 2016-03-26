f1366f9511a1bf3417eb8ccd9453981dc3ed7ff6
Darkchicken <joshshucker@hotmail.com>
2016-03-26 13:28:42 -0700

    Created a handler for musical cross fades
    Created an audio handler to hold all playing audio sources
    Created a trigger object that starts the cross fade

---
+++ Assets/Scripts/CrossFadeControl.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossFadeControl : MonoBehaviour {
+
+    public AudioSource audio1, audio2;
+    public AudioClip[] musicList;
+    public AudioClip startingMusic;
+    
+    
+	// Use this for initialization
+	void Start ()
+    {
+        
+        audio1.volume = 1;
+        audio2.volume = 0;
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    //pass a clip to fade in to
+    public void CrossFade(AudioClip newClip)
+    {
+        //if audio 1 is the source playing currently, fade into 2
+        if (audio1.volume == 1)
+        {
+            //if audio 1 is playing, reassign audio 2 to the new clip
+            //audio2.clip = newClip;
+            StartCoroutine("ChangeMusic1");
+        }
+        else
+        {
+            //if audio 2 is playing, reassign audio 1 to the new clip
+            //audio1.clip = newClip;
+            StartCoroutine("ChangeMusic2");
+        }
+    }
+    // one fades out, two fades in
+    private IEnumerator ChangeMusic1()
+    {
+        float fTimeCounter = 0f;
+
+        while (!(Mathf.Approximately(fTimeCounter, 1f)))
+        {
+            fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
+            audio1.volume = 1f - fTimeCounter;
+            audio2.volume = fTimeCounter;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        StopCoroutine("ChangeMusic");
+    }
+    // two fades out, one fades in
+    private IEnumerator ChangeMusic2()
+    {
+        float fTimeCounter = 0f;
+
+        while (!(Mathf.Approximately(fTimeCounter, 1f)))
+        {
+            fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
+            audio2.volume = 1f - fTimeCounter;
+            audio1.volume = fTimeCounter;
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        StopCoroutine("ChangeMusic");
+    }
+}
---
+++ Assets/Scripts/CrossFadeTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossFadeTrigger : MonoBehaviour {
+
+    public GameObject audioHandler;
+    CrossFadeControl handlerScript;
+    public AudioClip clipToPlay;
+	// Use this for initialization
+	void Start ()
+    {
+        handlerScript = audioHandler.GetComponent<CrossFadeControl>();
+	}
+	
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        //if the player collides with this object
+        if(col.tag == "Player")
+        {
+            //activate cross fade in audio handler
+            handlerScript.CrossFade(clipToPlay);
+        }
+    }
+}
