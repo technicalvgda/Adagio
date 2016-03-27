297cd139ea524d680cdffc2fddc67588f13f557c
Darkchicken <joshshucker@hotmail.com>
2016-03-27 12:13:59 -0700

    More additions for crossfade
    Audiohandler now crossfades 3 tracks, added temp music to test function

--- Assets/Scripts/CrossFadeControl.cs
+++ Assets/Scripts/CrossFadeControl.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class CrossFadeControl : MonoBehaviour {
 
-    public AudioSource audio1, audio2;
-    public AudioClip[] musicList;
+    public AudioSource audio1, audio2, audio3;
     public AudioClip startingMusic;
     
     
@@ -14,60 +13,82 @@ public class CrossFadeControl : MonoBehaviour {
         
         audio1.volume = 1;
         audio2.volume = 0;
-        
+        audio3.volume = 0;
+
 
     }
 	
-	// Update is called once per frame
-	void Update ()
-    {
 	
-	}
-    //pass a clip to fade in to
-    public void CrossFade(AudioClip newClip)
+    public void CrossFadeOneTwo()
     {
-        //if audio 1 is the source playing currently, fade into 2
-        if (audio1.volume == 1)
-        {
-            //if audio 1 is playing, reassign audio 2 to the new clip
-            //audio2.clip = newClip;
-            StartCoroutine("ChangeMusic1");
-        }
-        else
-        {
-            //if audio 2 is playing, reassign audio 1 to the new clip
-            //audio1.clip = newClip;
-            StartCoroutine("ChangeMusic2");
-        }
+       
+        StartCoroutine("ChangeMusicOneAndTwo");
+       
+    }
+
+    public void CrossFadeTwoThree()
+    {
+
+        StartCoroutine("ChangeMusicTwoAndThree");
+
     }
     // one fades out, two fades in
-    private IEnumerator ChangeMusic1()
+    private IEnumerator ChangeMusicOneAndTwo()
     {
-        float fTimeCounter = 0f;
+        AudioSource fadeOut = null, fadeIn = null;
+        if (audio1.volume > 0)
+        {
+            fadeOut = audio1;
+            fadeIn = audio2;
+        }
+        else if (audio2.volume > 0)
+        {
+            fadeOut = audio2;
+            fadeIn = audio1;
+        }
+        else//if both sources are silent (this shouldnt happen)
+        { StopCoroutine("ChangeMusic"); }
 
+        float fTimeCounter = 0f;
+       
         while (!(Mathf.Approximately(fTimeCounter, 1f)))
         {
             fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
-            audio1.volume = 1f - fTimeCounter;
-            audio2.volume = fTimeCounter;
+            fadeOut.volume = 1f - fTimeCounter;
+            fadeIn.volume = fTimeCounter;
             yield return new WaitForSeconds(0.02f);
         }
 
         StopCoroutine("ChangeMusic");
     }
-    // two fades out, one fades in
-    private IEnumerator ChangeMusic2()
+    // one fades out, two fades in
+    private IEnumerator ChangeMusicTwoAndThree()
     {
+        AudioSource fadeOut = null,fadeIn = null;
+        if (audio2.volume > 0 )
+        {
+            fadeOut = audio2;
+            fadeIn = audio3;
+        }
+        else if (audio3.volume > 0)
+        {
+            fadeOut = audio3;
+            fadeIn = audio2;
+        }
+        else//if both sources are silent (this shouldnt happen)
+        {StopCoroutine("ChangeMusic");}
+
         float fTimeCounter = 0f;
 
         while (!(Mathf.Approximately(fTimeCounter, 1f)))
         {
             fTimeCounter = Mathf.Clamp01(fTimeCounter + Time.deltaTime);
-            audio2.volume = 1f - fTimeCounter;
-            audio1.volume = fTimeCounter;
+            fadeOut.volume = 1f - fTimeCounter;
+            fadeIn.volume = fTimeCounter;
             yield return new WaitForSeconds(0.02f);
         }
 
         StopCoroutine("ChangeMusic");
     }
+
 }
