dff1bb345bca633cec7017888b2dce3717afb125
Ken Miller <ken_miller521@yahoo.com>
2016-03-31 19:36:46 -0700

    Updated foreground and code
    -Added more foreground assets
    -Updated the code for the foreground so that the foreground moves
    slower. Also made it so if the player is moving back and forth quickly
    the foreground will not move back and forth quickly also.

--- Assets/Scripts/ForegroundMovement.cs
+++ Assets/Scripts/ForegroundMovement.cs
@@ -4,33 +4,37 @@ using System.Collections;
 public class ForegroundMovement : MonoBehaviour
 {
 
-    float maxX = 200f;
-    float minX = 0f;
-    float foregroundHeight = 100f;
-    public GameObject player;
+	float maxX = 200f;
+	float minX = 0f;
+	float foregroundHeight = 100f;
+	public GameObject player;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (player == null)
-        {
-            player = GameObject.Find("Player(Clone)");
-        }
-        else
-        {
-            if (player.transform.position.x > 0 && player.transform.position.x < 200)
-            {
-                float playerValue = player.transform.position.x / 200;
+	// Update is called once per frame
+	void Update()
+	{
+		if (player == null)
+		{
+			player = GameObject.Find("Player(Clone)");
+		}
+		else
+		{
+			if (player.GetComponent<PlayerController> ().moveHorizontal > -0.875 && player.GetComponent<PlayerController> ().moveHorizontal < 0.875) 
+			{
+				//Do nothing
+			}
+			else if (player.transform.position.x > 0 && player.transform.position.x < 200) 
+			{
+				float playerValue = player.transform.position.x / 200;
 
-                transform.position = Vector2.Lerp(new Vector2(maxX, foregroundHeight), new Vector2(minX, foregroundHeight), playerValue);
-            }
+				transform.position = Vector2.Lerp (new Vector2 (maxX, foregroundHeight), new Vector2 (minX, foregroundHeight), playerValue / 5);
+			}
 
-        }
-    }
+		}
+	}
 }
