3dc0b128a1e452ffb0c512079dd22303dcc26125
j95alvarez <juanalvarez391@yahoo.com>
2016-04-10 00:53:08 -0700

    Chime Puzzle
    The player needs to hit each gold chime, in order, from left to right.
    If the player hits the red chime, the puzzle will reset.

---
+++ Assets/ChimeReset.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChimeReset : MonoBehaviour {
+
+    public bool stepped;
+     int state = 0;
+     public bool pressed;
+ 
+     void Start()
+     {
+ 
+         stepped = false;
+         pressed = false;
+ 
+     }
+ 
+     void Update()
+     {
+         // If the player is colliding with the button, advance the state.
+         // since Update() executes more frequently and is less likely to cause missed checks.
+         if (stepped)
+         {
+             Debug.Log("ON");
+             pressed = true;
+         }
+     }
+ 
+     // NOTE: Set the Player object's RigidBody2D -> Sleeping Mode to Never Sleep, otherwise
+     // the collision check will eventually stop due to the player object going to sleep.
+     void OnTriggerStay2D(Collider2D other)
+     {
+         if (other.gameObject.tag == "Player")
+             stepped = true;
+     }
+ 
+     void OnTriggerExit2D(Collider2D other)
+     {
+         if (other.gameObject.tag == "Player")
+             stepped = false;
+     }
+}
