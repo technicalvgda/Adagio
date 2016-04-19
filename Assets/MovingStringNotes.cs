02e2cdb608c615c2df3e1a55390c6a8d5442b9a3
j95alvarez <juanalvarez391@yahoo.com>
2016-04-19 15:34:47 -0700

    Moving Note (Fixed)
    Player needs to match the notes displayed on the top right (from left to
    right). Each moves at different speeds that can be changed, if needed
    (USING FLOAT).

---
+++ Assets/MovingStringNotes.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingStringNotes : MonoBehaviour
+{
+
+    public Transform point1;
+    Vector3 start;
+    Vector3 end;
+    public float speed = .05f;
+    bool x = true;
+    int counter = 0;
+
+
+    void Update()
+    {
+      
+        if (counter == 0)
+        {
+            start = new Vector3(point1.position.x, point1.position.y - 6.5f, point1.position.z);
+            end = new Vector3(point1.position.x, point1.position.y + 6.5f, point1.position.z);
+        }
+
+        if (point1.position == end)
+        {
+            x = false;
+        }
+        else if (point1.position == start)
+        {
+            x = true;
+        }
+        if (point1.position == end || x == false)
+        {
+            transform.position = Vector3.MoveTowards(point1.position, start, speed);
+            counter += 1;
+        }
+        else if (point1.position != end)
+        {
+            transform.position = Vector3.MoveTowards(point1.position, end, speed);
+            counter += 1;
+        }
+    }
+}
