ebd982c853e6795ada3cfc76534a8758be6a736e
Doan D <tamitystg@gmail.com>
2016-04-21 14:55:25 -0700

    Title screen (incomplete)
    IMPORTANT:
    - Build it and run the .exe. This will (hopefully) avoid hiccups by the
    editor. For extra measure, run it once beforehand so that things are
    cached. It's very important that it doesn't hiccup during the intro.
    
    Status:
    - The following buttons don't do anything yet: Start, Music, Sounds.
    Image/Brightness setting also lacks a preview.
    - No follow-up to other scenes yet
    - Sliders don't do anything yet but their logic are already written

---
+++ Assets/Scenes/UI/script/AdagioUIElement.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AdagioUIElement : MonoBehaviour
+{
+    public virtual void Init() { }
+    public virtual void Enter() { this.gameObject.SetActive(true); }
+    public virtual void Exit() { this.gameObject.SetActive(false); }
+}
---
+++ Assets/Scenes/UI/script/AnimSymbols.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimSymbols : MonoBehaviour
+{
+    public Animation[] symbols;
+
+    public void LightUp(int symbol)
+    {
+        symbol = Mathf.Clamp(symbol, 0, symbols.Length);
+        symbols[symbol].Play("animSymbolLightUp", PlayMode.StopAll);
+    }
+
+    public void TurnToDot(int symbol)
+    {
+        symbol = Mathf.Clamp(symbol, 0, symbols.Length);
+        symbols[symbol].Play("animSymbolToDot", PlayMode.StopAll);
+    }
+
+    //in your dreams
+    //public Animation[] anims;
+    //public string[] clips;
+    //public void Play(int animIndex, int clipIndex) {}
+}
