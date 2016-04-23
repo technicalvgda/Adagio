06522955e0da502bd3f8fb77e57cb93792c54663
Darkchicken <joshshucker@hotmail.com>
2016-04-23 14:26:50 -0700

    Added Fresco
    Added fresco tiles, placed in scene
    Some tiles are placeholders
    Added a door that autoshuts when you exit hub

---
+++ Assets/Scripts/HubDoor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+/* Basic script to recieve a button press. 
+ * Is attached to the button in the button's inspection window.
+ */
+
+public class HubDoor : MonoBehaviour
+{
+
+    float initialYPos;
+    float endingYPos;
+    float movmentValue = 3f;
+    void Start()
+    {
+        Deactivate();
+        initialYPos = transform.position.y;
+        endingYPos = initialYPos - movmentValue;
+    }
+    public void Deactivate()
+    {
+        StartCoroutine(LowerDoor());
+        //gameObject.SetActive (false);
+    }
+    public void Activate()
+    {
+        StartCoroutine(RaiseDoor());
+    }
+
+    private IEnumerator LowerDoor()
+    {
+        while (transform.position.y > endingYPos)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.1f);
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
+    private IEnumerator RaiseDoor()
+    {
+        while (transform.position.y < initialYPos)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.1f);
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
+
+
+}
