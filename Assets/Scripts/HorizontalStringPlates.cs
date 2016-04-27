11f560d8952449ff38b20622c7e060c835d3651b
Darkchicken <joshshucker@hotmail.com>
2016-04-26 18:21:11 -0700

    Horizontal String puzzle complete

---
+++ Assets/Scripts/HorizontalStringPlates.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalStringPlates : MonoBehaviour {
+
+    public GameObject horizontalPuzzle;
+    HorizontalStringPuzzle puzzleScript;
+    public string plateName;
+    bool activateButton = true;
+    void Start()
+    {
+        puzzleScript = horizontalPuzzle.GetComponent<HorizontalStringPuzzle>();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player" && activateButton == true)
+        {
+            activateButton = false;
+            StartCoroutine(DelayButton());
+            puzzleScript.MoveNote(plateName);
+        }
+    }
+
+    IEnumerator DelayButton()
+    {
+        yield return new WaitForSeconds(1);
+        activateButton = true;
+        yield return null;
+    }
+}
