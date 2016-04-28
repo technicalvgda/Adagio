472277c3feb8ff53c12ab6fc8f867ea63a2dc99c
Doan D <tamitystg@gmail.com>
2016-04-28 01:22:14 -0700

    Finished title screen button bar + loading screen
    Title screen
    - Go to the "STAGE CONTROL" object and put the game scene's build index
    into the "Start Index" field. The scene at this index will be loaded
    when the Start button is pressed.
    - There is a ~3-second "safe zone" at the beginning of the intro. After
    this, pressing the left mouse button will skip to the logo and button
    bar.
    
    Loading screen
    - The files are LoadingB.unity (scene) and AdagioLoad.cs (script), a
    more polished version of the existing SceneLoader.cs.
    - In order to load, AdagioLoad.destination must be set to the desired
    scene's build index following by LoadScene of the loading scene itself.
    Invalid destination values will redirect you to the title screen.
    AdagioLoad.destination = /*index of target scene*/;
    SceneManager.LoadScene(/*index of loading scene*/);
    - The "Loading" pane is ONLY shown if the load takes longer than 1.5
    seconds (the loading pane is an Animation component with 1.5 seconds of
    nothing at the beginning, this can be changed)

---
+++ Assets/Scenes/UI/script/AdagioLoad.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class AdagioLoad : MonoBehaviour
+{
+    // From another scene: MUST set AdagioLoad.destination to a valid scene ID, then load this scene
+    public static int destination = -1;
+
+    public SpriteRenderer spriteFade;
+    public Transform spriteLoading;
+
+    IEnumerator Start()
+    {
+        if (destination < 0 ||
+            destination >= SceneManager.sceneCountInBuildSettings ||
+            destination == SceneManager.GetActiveScene().buildIndex)
+        {
+            destination = 0;
+        }
+
+        AsyncOperation load = SceneManager.LoadSceneAsync(destination);
+        load.allowSceneActivation = false;
+
+        destination = -1;
+
+        // The new scene is swapped in when progress is over 90% (apparently 90% counts as "ready")
+        // !allowSceneActivation stalls it at 90% instead and it stays there until the boolean is true
+        while (load.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        // The scene is supposedly 100% loaded (not 90%) when isDone is true;
+        // this will never happen if allowSceneActivation is false
+        //while (!load.isDone)
+        //{
+        //    yield return null;
+        //}
+
+        // The loading pane only shows up if the load has taken long enough (default: 1.5s)
+        // Change this behavior in the attached animation clip
+
+        // Load over, fade out
+        this.GetComponent<Animation>().Stop();
+        for (int i = 0; i < 10; i++)
+        {
+            if (spriteFade.color.a < 1)
+                spriteFade.color += new Color(0, 0, 0, 0.1f);
+            else spriteFade.color = Color.black;
+
+            spriteLoading.localScale -= new Vector3(0.001f, 0.001f, 0);
+
+            yield return null;
+        }
+
+        // Setting this to true allows the scene to be switched
+        load.allowSceneActivation = true;
+    }
+}
--- Assets/Scenes/UI/script/AdagioTitleCtrl.cs
+++ Assets/Scenes/UI/script/AdagioTitleCtrl.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 // Manages the flow of the screens, holds public functions to be called by buttons
@@ -19,6 +20,12 @@ public class AdagioTitleCtrl : MonoBehaviour
     [Header("First menu")]
     public AdagioMenu menuTitle;
     AdagioMenu menuCurrent;
+
+    [Space]
+    public int startIndex = 0;
+
+    WaitForSeconds delay05s = new WaitForSeconds(0.5f);
+    WaitForSeconds delay1s = new WaitForSeconds(1);
 
     void Start()
     {
@@ -62,25 +69,29 @@ public class AdagioTitleCtrl : MonoBehaviour
         objIntro.SetActive(true);
         objLogo.SetActive(true);
         objButtonBar.SetActive(true);
-        yield return null;
+        menuTitle.Enter();
+
+        yield return delay05s;
+
         objIntro.SetActive(false);
         objLogo.SetActive(false);
         objButtonBar.SetActive(false);
+        menuTitle.Exit();
 
-        yield return null;
+        yield return delay05s;
 
         // Remove curtain
         spriteFade.color = Color.clear;
 
-        for (int i = 0; i < 60; i++) yield return null;
+        yield return delay1s;
     }
 
-    WaitForSeconds delay05s = new WaitForSeconds(0.5f);
-    WaitForSeconds delay1s = new WaitForSeconds(1);
     IEnumerator _Run()
     {
+        // Enable only one
         //yield return _ForceLoad();
         yield return delay05s;
+        //yield return delay1s;
 
         objIntro.SetActive(true);
         audioCtrl.PlayBgm(0, true);
@@ -91,7 +102,7 @@ public class AdagioTitleCtrl : MonoBehaviour
 
         for (int i = 0; i < 1380; i++) // +1380
         {
-            if (Input.anyKeyDown)
+            if (Input.GetKeyDown(KeyCode.Mouse0) )
             {
                 yield return SkipIntro();
                 yield return LogoSequence();
@@ -100,7 +111,7 @@ public class AdagioTitleCtrl : MonoBehaviour
             yield return null;
         }
 
-        yield return new WaitForSeconds(2); // +120
+        yield return new WaitForSeconds(2.1f); // +~120
         yield return LogoSequence();
     }
 
@@ -127,9 +138,13 @@ public class AdagioTitleCtrl : MonoBehaviour
     {
         objLogo.SetActive(true);
 
-        yield return new WaitForSeconds(2.5f);
+        yield return delay1s;
+        //deactivating Intro object while thin symbol on logo is fading out causes it to flicker?????????????????????
+        //the fade out is between 2.5s to 3.1s. yield outside of this range
 
         objIntro.SetActive(false);
+
+        yield return delay1s;
 
         objButtonBar.SetActive(true);
 
@@ -146,9 +161,38 @@ public class AdagioTitleCtrl : MonoBehaviour
         menuCurrent = menu;
     }
 
-    public void Quit()
+    public void StartGame() { StartCoroutine(_StartGame() ); }
+    IEnumerator _StartGame()
     {
-        print("User quits");
+        // a method called StartGame is not supposed to do this but
+        // it isn't like there's a black fade/audio fade anywhere else (lol
+
+        while (spriteFade.color.a < 1)
+        {
+            spriteFade.color += new Color(0, 0, 0, 0.05f);
+            AudioListener.volume -= 0.05f;
+            yield return null;
+        }
+        spriteFade.color = Color.black;
+
+        // Audio listener is global, so reenable it to avoid fucking up other scenes
+        AudioListener.volume = 1;
+
+        // Now there's an audible glitch at LoadScene, so mute the audio sources
+        // These will just get reset on a scene load
+        audioCtrl.SetBgmVolume(0);
+        audioCtrl.SetSeVolume(0);
+
+        yield return null;
+
+        // Set this to the desired scene's build index; invalid values will default to title
+        AdagioLoad.destination = startIndex;
+        // Loading the load scene after this will bring you to the destination scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
         Application.Quit();
     }
 }
