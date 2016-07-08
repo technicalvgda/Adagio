1d9bde9274a0c650afeb99c734346c957cb9d6c2
Darkchicken <joshshucker@hotmail.com>
2016-07-08 14:08:16 -0700

    Began Work on Codecies menu

---
+++ Assets/Scripts/CodexHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CodexHandler : MonoBehaviour {
+
+
+    //EVENTS
+    public delegate void AnswerCallback(string message);
+
+    // Event declaration
+    public event AnswerCallback OnUnityAnswers;
+
+
+    GameObject textPanel;
+    Text codexText;
+
+    int numberOfCodecies;
+	// Use this for initialization
+	void Start ()
+    {
+        //get text component on codex text panel
+        codexText = textPanel.GetComponent<Text>();
+        //make sure text panel is inactive
+        textPanel.SetActive(false);
+        //numberOfCodecies = 
+	
+        if(!PlayerPrefs.HasKey("Codecies"))
+        {
+            //string
+           // PlayerPrefs.SetString();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
