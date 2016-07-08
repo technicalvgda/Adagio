7773a4ffb3f5ef2e074a5b48603c1dff15c9d50e
ken_miller <ken_miller521@yahoo.com>
2016-07-08 01:56:36 -0700

    Made tutorial entrance door go up + new tutorial
    -Changed TrapDoorOpenResponse by adding a bool that when true the door
    will go up instead of down
    -Made the new tutorial area. I did not replace the old tutorial in the
    generation scene to prevent any merge requests. It needs to merge with
    the hub after all other pull requests are merged.

--- Assets/Scripts/TrapDoorOpenResponse.cs
+++ Assets/Scripts/TrapDoorOpenResponse.cs
@@ -5,14 +5,18 @@ using System.Collections;
  */
 
 public class TrapDoorOpenResponse : MonoBehaviour {
-
+	public bool goUp;
     float initialYPos;
     float endingYPos;
     float movmentValue = 3f;
     void Start()
     {
         initialYPos = transform.position.y;
-        endingYPos = initialYPos - movmentValue;
+
+		if(goUp)
+			endingYPos = initialYPos + movmentValue;
+		else
+        	endingYPos = initialYPos - movmentValue;
     }
 	public void Deactivate()
     {
@@ -26,20 +30,36 @@ public class TrapDoorOpenResponse : MonoBehaviour {
 
     private IEnumerator LowerDoor()
     {
-        while(transform.position.y > endingYPos)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y-0.1f);
-            yield return new WaitForSeconds(0.1f);
-        }
+		if (goUp) {
+			while (transform.position.y < endingYPos) {
+				transform.position = new Vector2 (transform.position.x, transform.position.y + 0.02f);
+				yield return new WaitForSeconds (0.02f);
+			}
+		} 
+		else 
+		{
+			while (transform.position.y > endingYPos) {
+				transform.position = new Vector2 (transform.position.x, transform.position.y - 0.02f);
+				yield return new WaitForSeconds (0.02f);
+			}
+		}
         yield return null;
     }
     private IEnumerator RaiseDoor()
     {
-        while (transform.position.y < initialYPos)
-        {
-            transform.position = new Vector2(transform.position.x, transform.position.y + 0.1f);
-            yield return new WaitForSeconds(0.1f);
-        }
+		if (goUp) {
+			while (transform.position.y < initialYPos) {
+				transform.position = new Vector2 (transform.position.x, transform.position.y + 0.02f);
+				yield return new WaitForSeconds (0.02f);
+			}
+		} 
+		else 
+		{
+			while (transform.position.y > initialYPos) {
+				transform.position = new Vector2 (transform.position.x, transform.position.y - 0.02f);
+				yield return new WaitForSeconds (0.02f);
+			}
+		}
         yield return null;
     }
 
