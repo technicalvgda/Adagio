ef76b63b6d7380f0025ac40a943d25a9ecec48b8
ken_miller <ken_miller521@yahoo.com>
2016-08-14 15:33:06 -0700

    5 Rings Puzzle Implemented
    Finished the main puzzle. Other effects,such as the sparkle, are to be
    added once assets are ready.

---
+++ Assets/Scripts/FiveRingsPuzzleManager.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FiveRingsPuzzleManager : MonoBehaviour {
+	//The ring that the player collides to start the puzzle
+	public GameObject startRing;
+	//The rings that move
+	public GameObject ring1, ring2, ring3,ring4;
+	//The blocks prefabs that surround the staring ring
+	public GameObject theBlockWithBanner,theBlock;
+	//the list of blocks that surround the ring in the center
+	public List<GameObject> surroundingBlocks;
+	//Bool for if the puzzle has started
+	private bool puzzleStarted;
+	//Bool for if the puzzle is complete
+	private bool complete;
+	//bool needed to execute each case statement once
+	private bool finishedCaseStatement;
+	//The number of times the player has jumped midair
+	private int jumpCounter;
+	//The raycasting script on the player
+	private DirectionRaycasting2DCollider rayCast;
+	private GameObject player;
+	//The array of locations where the rings will travel to
+	public GameObject[] patternLocations;
+	//the number of rings collected
+	private int numbRingsCollected;
+	//bool for if the puzzle has been reset
+	private bool puzzleHasReset;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		puzzleStarted = false;
+		rayCast = player.GetComponent<DirectionRaycasting2DCollider> ();
+		jumpCounter = 0;
+		surroundingBlocks = new List<GameObject> ();
+		finishedCaseStatement = false;
+		ring1.SetActive (false);
+		ring2.SetActive (false);
+		ring3.SetActive (false);
+		ring4.SetActive (false);
+		complete = false;
+		puzzleHasReset = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		//if the player has collected all the moving rings and the ring in the center, then the player has finished the puzzle
+		if (numbRingsCollected == 5 && !complete) 
+		{
+			Debug.Log ("You have finished the 5 Ring Puzzle");
+			complete = true;
+			Camera.main.GetComponent<OpenGate>().doneCounter++;
+		}
+		else if (numbRingsCollected == 4) 
+		{
+			//collected all the rings
+			//deactivate the surrounding blocks
+			for (int i = 0; i < surroundingBlocks.Count; i++) 
+			{
+				surroundingBlocks [i].SetActive (false);
+			}
+			//reset the radius of the ring in the center
+			startRing.GetComponent<CircleCollider2D>().radius = 5;
+		}
+		//Puzzle started but not complete
+		if (puzzleStarted && !complete) 
+		{
+			//If the player is standing, then reset the puzzle
+			if (rayCast.collisionDown && !puzzleHasReset) 
+			{
+				resetPuzzle ();
+				puzzleHasReset = true;
+			}
+
+			if (Input.GetKeyDown (KeyCode.Space)) 
+			{
+				//if the player does a jump midair
+				if (!rayCast.collisionDown) 
+				{
+					jumpCounter++;	
+					Debug.Log (jumpCounter);
+					//set to false so it can execute the next case statement
+					finishedCaseStatement = false;
+					puzzleHasReset = false;
+				}
+			}
+
+			switch (jumpCounter) 
+			{
+			case 1:
+				{	
+					if (finishedCaseStatement == false) {
+						//surround the ring with blocks, simultaneously spawn them and add them to a list for later deletion
+						surroundingBlocks.Add ((GameObject)Instantiate (theBlockWithBanner, new Vector3 (transform.position.x + 1, transform.position.y, transform.position.z), Quaternion.identity));
+						surroundingBlocks.Add ((GameObject)Instantiate (theBlockWithBanner, new Vector3 (transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity));
+						surroundingBlocks.Add ((GameObject)Instantiate (theBlockWithBanner, new Vector3 (transform.position.x - 1, transform.position.y, transform.position.z), Quaternion.identity));
+						surroundingBlocks.Add ((GameObject)Instantiate (theBlock, new Vector3 (transform.position.x, transform.position.y - 1, transform.position.z), Quaternion.identity));
+						//set to true to avoid executing this case statement continuously
+						finishedCaseStatement = true;
+					}
+					break;
+				}
+			case 2:
+				{
+					if (finishedCaseStatement == false) 
+					{
+						ring1.SetActive (true);
+						finishedCaseStatement = true;
+					}
+					break;
+				}
+			case 3:
+				{
+					if (finishedCaseStatement == false) 
+					{					
+						ring1.GetComponent<FiveRingsPuzzleRingScript> ().pattern1Begin ();
+						finishedCaseStatement = true;
+					}
+					break;
+				}
+			case 4:
+				{
+					if (finishedCaseStatement == false)
+					{
+						ring2.SetActive (true);
+						ring2.GetComponent<FiveRingsPuzzleRingScript> ().reverseTraversal = true;
+						ring2.GetComponent<FiveRingsPuzzleRingScript> ().pattern1Begin ();
+						finishedCaseStatement = true;
+					}
+					break;
+				}
+			case 5:
+				{	if (finishedCaseStatement == false) 
+					{
+						ring3.SetActive(true);
+						ring4.SetActive(true);
+						ring3.GetComponent<FiveRingsPuzzleRingScript> ().pattern2Begin ();
+						ring4.GetComponent<FiveRingsPuzzleRingScript> ().reverseTraversal = true;
+						ring4.GetComponent<FiveRingsPuzzleRingScript> ().pattern2Begin ();
+							finishedCaseStatement = true;
+					}
+					break;
+				}
+			}		
+		}	
+	}
+	//function that starts the puzzle
+	public void startPuzzle()
+	{
+		puzzleStarted = true;
+		startRing.transform.position = transform.position;
+		//reduce radius so that the player won't accidentaly restart the puzzle by touching the collider, NEED TO RESET BACK TO 5 UPON PUZZLE COMPLETION
+		startRing.GetComponent<CircleCollider2D>().radius = 1;
+	}
+	//returns the pattern locations
+	public GameObject[] getPatternLocations()
+	{
+		return patternLocations;
+	}
+	public void collectedRing()
+	{
+		numbRingsCollected++;
+	}
+	//returns the number of rings collected
+	public int getNumbRingsCollected()
+	{
+		return numbRingsCollected;
+	}
+	//function to reset the puzzle
+	private void resetPuzzle()
+	{
+		//reactivate the first ring
+		ring1.SetActive (true);
+		//reset positions of each ring and deactivate movement
+		ring1.GetComponent<FiveRingsPuzzleRingScript>().resetRing();
+		ring2.GetComponent<FiveRingsPuzzleRingScript>().resetRing();
+		ring3.GetComponent<FiveRingsPuzzleRingScript>().resetRing();
+		ring4.GetComponent<FiveRingsPuzzleRingScript>().resetRing();
+		//deactivate all rings except for ring1
+		ring2.SetActive(false);
+		ring3.SetActive(false);
+		ring4.SetActive(false);
+		//reactivate the blocks surrounding the starting ring
+		for (int i = 0; i < surroundingBlocks.Count; i++) {
+			surroundingBlocks [i].SetActive (true);
+		}
+		jumpCounter = 2;
+		finishedCaseStatement = false;
+		numbRingsCollected = 0;
+	}
+}
---
+++ Assets/Scripts/FiveRingsPuzzleRingScript.cs
@@ -0,0 +1,204 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FiveRingsPuzzleRingScript : MonoBehaviour {
+	//The manager for the puzzle
+	private GameObject fiveRingsManager;
+	//bools for pattern1, pattern 2, and if the ring is moving
+	private bool pattern1Init,pattern2Init, isMoving;
+	public GameObject[] locations;
+	//the speed of the ring
+	public float speed;
+	//bool for if the ring ISerializationCallbackReceiver to travel the opposite way
+	public bool reverseTraversal;
+	//the counter to keep track of what location is next for the ring to move towards
+	private int locationCounter;
+	public float frequency;//Speed of sine movement
+	public float magnitude;//Size of sine movement
+
+	//some variables to make the ring move in a sine wave
+	private Vector3 axis, pos;
+	float fTime = 0;
+	private Transform target;
+	public float rotationSpeed;
+	private Quaternion lookRotation;
+	private Vector3 dir;
+
+	//the initial location where the ring started
+	private Vector3 beginningPos;
+	// Use this for initialization
+	void Start () {
+		fiveRingsManager = GameObject.Find ("5StringManager");
+		pattern1Init = false;
+		pattern2Init = false;
+		locationCounter = 0;
+		reverseTraversal = false;
+		pos = transform.position;
+		beginningPos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isMoving) 
+		{
+			//initialize pattern 1 movement
+			if (pattern1Init) {
+				isMoving = true;
+				StartCoroutine ("Pattern1Traversal");
+			} 
+			else if (pattern2Init) 
+			{
+				isMoving = true;
+				StartCoroutine ("Pattern2Traversal");
+			}
+		}
+	
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (gameObject.name == "StartRing") 
+		{
+			if (other.gameObject.tag == "Player") 
+			{
+				//if the player has collected all the other rings
+				if (fiveRingsManager.GetComponent<FiveRingsPuzzleManager> ().getNumbRingsCollected() == 4) 
+				{
+					gameObject.SetActive (false);
+					fiveRingsManager.GetComponent<FiveRingsPuzzleManager> ().collectedRing ();
+				}
+				else
+					fiveRingsManager.GetComponent<FiveRingsPuzzleManager> ().startPuzzle ();
+			}
+		} 
+		else 
+		{
+			gameObject.SetActive (false);
+			fiveRingsManager.GetComponent<FiveRingsPuzzleManager> ().collectedRing ();
+		}
+	}
+
+	public void pattern1Begin()
+	{
+		pattern1Init = true;
+		locations = fiveRingsManager.GetComponent<FiveRingsPuzzleManager> ().getPatternLocations ();
+	}
+
+	public void pattern2Begin()
+	{
+		pattern2Init = true;
+		locations = fiveRingsManager.GetComponent<FiveRingsPuzzleManager> ().getPatternLocations ();
+	}
+	//HAndles the movement, forward and reversed traversal of the rings
+	IEnumerator Pattern1Traversal()
+	{
+		if (!reverseTraversal) {
+			while (transform.position != locations [locationCounter].transform.position) {
+				transform.position = Vector3.MoveTowards (transform.position, locations [locationCounter].transform.position, speed * Time.deltaTime);
+				if (transform.position == locations [locationCounter].transform.position) {
+					locationCounter++;
+					if (locationCounter == locations.Length)
+						locationCounter = 0;
+					//break;
+				}
+				yield return null;
+			}
+		} 
+		else 
+		{
+			locationCounter = 4;
+			while (transform.position != locations [locationCounter].transform.position) {
+				transform.position = Vector3.MoveTowards (transform.position, locations [locationCounter].transform.position, speed * Time.deltaTime);
+				if (transform.position == locations [locationCounter].transform.position) {
+					locationCounter--;
+					if (locationCounter < 0) {
+						locationCounter = locations.Length-1;
+					}
+					//break;
+				}
+				yield return null;
+			}
+		}
+		yield return null;
+	}
+
+	//hamdles the sine wave movement for the rings
+	IEnumerator Pattern2Traversal()
+	{
+		
+		if (!reverseTraversal) 
+		{
+			locationCounter = 6;
+			while (transform.position != locations [locationCounter].transform.position) 
+			{
+				//rotates the ring towards the next location
+				Vector3 vectorToTarget = locations [locationCounter].transform.position - transform.position;
+				float angle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+				Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+				transform.rotation = Quaternion.Slerp (transform.rotation, q, Time.deltaTime * rotationSpeed);
+				//handles the math for the sine wave
+				pos += transform.right * Time.deltaTime * speed;
+				axis = transform.up;
+				transform.position = pos + axis * Mathf.Sin (Time.time * frequency) * magnitude;
+				Debug.DrawLine (pos, transform.position, Color.green, 100);
+
+				if (transform.position.x > (locations [locationCounter].transform.position.x - 0.5f) && transform.position.x < (locations [locationCounter].transform.position.x + 0.5f)) 
+				{
+					if (transform.position.y > (locations [locationCounter].transform.position.y - 0.25f) && transform.position.y < (locations [locationCounter].transform.position.y + 0.25f)) 
+					{
+						transform.position = locations [locationCounter].transform.position;
+						locationCounter--;
+						if (locationCounter < 0) {
+							locationCounter = locations.Length - 1;
+						}
+					}
+				}
+
+				yield return null;
+			}
+		} 
+		else 
+		{
+			locationCounter = 6;
+			while (transform.position != locations [locationCounter].transform.position) 
+			{
+				Vector3 vectorToTarget = locations [locationCounter].transform.position - transform.position;
+				float angle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+				Quaternion q = Quaternion.AngleAxis (angle, Vector3.forward);
+				transform.rotation = Quaternion.Slerp (transform.rotation, q, Time.deltaTime * rotationSpeed);
+
+				pos += transform.right * Time.deltaTime * speed;
+				axis = transform.up;				
+				transform.position = pos + axis * Mathf.Sin (Time.time * frequency) * magnitude;
+				Debug.DrawLine (pos, transform.position, Color.red, 100);
+
+				if (transform.position.x > (locations [locationCounter].transform.position.x - 0.25f) && transform.position.x < (locations [locationCounter].transform.position.x + 0.25f)) 
+				{
+					if (transform.position.y > (locations [locationCounter].transform.position.y - 0.25f) && transform.position.y < (locations [locationCounter].transform.position.y + 0.25f)) 
+					{
+						transform.position = locations [locationCounter].transform.position;
+						locationCounter++;
+						if (locationCounter == locations.Length) {
+							locationCounter = 0;
+						}
+					}
+				}
+
+				yield return null;
+			}
+		}
+		yield return null;
+	}
+	//function to reset values of the ring
+	public void resetRing()
+	{
+		transform.position = beginningPos;
+		pattern1Init = false;
+		pattern2Init = false;
+		locationCounter = 0;
+		reverseTraversal = false;
+		isMoving = false;
+		pos = transform.position;
+		StopAllCoroutines ();
+	}
+}
